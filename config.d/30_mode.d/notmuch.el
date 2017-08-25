337a700e6a0941fc85f6b242f5e0088cc9c774fe
Christian Menard <christian.menard@tu-dresden.de>
2017-08-25 15:37:04 +0200

    use use-package :config to configure notmuch
    this should increase startup time significantly

--- config.d/30_mode.d/notmuch.el
+++ config.d/30_mode.d/notmuch.el
@@ -1,55 +1,48 @@
 (use-package notmuch
   :ensure t
+  :bind ("C-x m" . notmuch)
   :config
-  (require 'notmuch))
+    (setq notmuch-search-oldest-first nil)
+    (define-key notmuch-show-mode-map "d"
+      (lambda ()
+        "toggle deleted tag for thread"
+        (interactive)
+        (if (member "deleted" (notmuch-show-get-tags))
+            (notmuch-show-tag (list "-deleted"))
+          (notmuch-show-tag (list "+deleted")))))
+    (define-key notmuch-search-mode-map "d"
+      (lambda ()
+        "toggle deleted tag for thread"
+        (interactive)
+        (if (member "deleted" (notmuch-search-get-tags))
+            (notmuch-search-tag (list "-deleted"))
+          (notmuch-search-tag (list "+deleted")))))
+    (define-key notmuch-show-mode-map "S"
+      (lambda ()
+        "mark message as spam"
+        (interactive)
+        (notmuch-show-tag (list "+spam" "-inbox"))))
 
-(setq notmuch-search-oldest-first nil)
-
-(define-key notmuch-show-mode-map "d"
-  (lambda ()
-    "toggle deleted tag for message"
-    (interactive)
-    (if (member "deleted" (notmuch-show-get-tags))
-        (notmuch-show-tag (list "-deleted"))
-      (notmuch-show-tag (list "+deleted")))))
-
-(define-key notmuch-search-mode-map "d"
-  (lambda ()
-    "toggle deleted tag for thread"
-    (interactive)
-    (if (member "deleted" (notmuch-search-get-tags))
-        (notmuch-search-tag (list "-deleted"))
-      (notmuch-search-tag (list "+deleted")))))
-
-(define-key notmuch-show-mode-map "S"
-  (lambda ()
-    "mark message as spam"
-    (interactive)
-    (notmuch-show-tag (list "+spam" "-inbox"))))
-
-(define-key notmuch-search-mode-map "S"
-  (lambda ()
-    "mark thread as spam"
-    (interactive)
-    (notmuch-search-tag (list "+spam" "-inbox"))))
-
-(define-key notmuch-show-mode-map "u"
-  (lambda ()
-    "toggle unread tag for message"
-    (interactive)
-    (if (member "unread" (notmuch-show-get-tags))
-        (notmuch-show-tag (list "-unread"))
-      (notmuch-show-tag (list "+unread")))))
-
-(define-key notmuch-search-mode-map "u"
-  (lambda ()
-    "toggle unread tag for message"
-    (interactive)
-    (if (member "unread" (notmuch-search-get-tags))
-        (notmuch-search-tag (list "-unread"))
-      (notmuch-search-tag (list "+unread")))))
-
-(define-key notmuch-search-mode-map "g"
-  'notmuch-poll-and-refresh-this-buffer)
-(define-key notmuch-hello-mode-map "g"
-  'notmuch-poll-and-refresh-this-buffer)
+    (define-key notmuch-search-mode-map "S"
+      (lambda ()
+        "mark thread as spam"
+        (interactive)
+        (notmuch-search-tag (list "+spam" "-inbox"))))
+    (define-key notmuch-show-mode-map "u"
+      (lambda ()
+        "toggle unread tag for message"
+        (interactive)
+        (if (member "unread" (notmuch-show-get-tags))
+            (notmuch-show-tag (list "-unread"))
+          (notmuch-show-tag (list "+unread")))))
+    (define-key notmuch-search-mode-map "u"
+      (lambda ()
+        "toggle unread tag for message"
+        (interactive)
+        (if (member "unread" (notmuch-search-get-tags))
+            (notmuch-search-tag (list "-unread"))
+          (notmuch-search-tag (list "+unread")))))
+    (define-key notmuch-search-mode-map "g"
+      'notmuch-poll-and-refresh-this-buffer)
+    (define-key notmuch-hello-mode-map "g"
+      'notmuch-poll-and-refresh-this-buffer))
