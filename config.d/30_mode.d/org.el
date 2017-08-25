9e9c8cf19aea3686638b36f4ca06245cc85b9a14
Christian Menard <christian.menard@tu-dresden.de>
2017-08-25 14:47:22 +0200

    use use-package for org and configure org-capture

--- config.d/30_mode.d/org.el
+++ config.d/30_mode.d/org.el
@@ -1,5 +1,13 @@
 ;; setup org mode
-(require 'org)
+(use-package org
+  :ensure org-plus-contrib
+  :config
+  (require 'org)
+  (require 'org-notmuch))
+
 (define-key global-map "\C-cl" 'org-store-link)
 (define-key global-map "\C-ca" 'org-agenda)
 (setq org-log-done t)
+
+(setq org-default-notes-file "~/tasks.org")
+(define-key global-map "\C-cc" 'org-capture)
