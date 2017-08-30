b140d7cbda748673c76a48bb8e47a11a1adee729
Christian Menard <christian.menard@tu-dresden.de>
2017-08-30 14:47:20 +0200

    Enable evil keybindings in notmuch

--- config.d/30_mode.d/evil.el
+++ config.d/30_mode.d/evil.el
@@ -5,4 +5,20 @@
   (evil-mode t)
   (evil-set-initial-state 'message-mode 'insert)
   (evil-set-initial-state 'notmuch-message-mode 'insert)
-  (setq evil-want-fine-undo t))
+  (setq evil-want-fine-undo t)
+
+  ;; Notmuch
+  (evil-define-key 'normal notmuch-search-mode-map
+    (kbd "q") 'notmuch-search-quit
+    (kbd "n") 'notmuch-search-next-thread
+    (kbd "p") 'notmuch-search-previous-thread
+    (kbd "RET") 'notmuch-search-show-thread)
+
+  ;; Mail
+  (evil-add-hjkl-bindings notmuch-search-mode-map 'emacs)
+  (evil-add-hjkl-bindings notmuch-show-mode-map 'emacs)
+  (evil-add-hjkl-bindings notmuch-hello-mode-map 'emacs)
+  (evil-set-initial-state 'notmuch-search-mode 'emacs)
+  (evil-set-initial-state 'notmuch-show-mode 'emacs)
+  (evil-set-initial-state 'notmuch-hello-mode 'emacs)
+  )
