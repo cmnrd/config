cca9310fd63966f95d8f4c0cd886e8ded104fb4c
Christian Menard <christian.menard@tu-dresden.de>
2017-08-30 13:48:07 +0200

    Improve the look and feel of org mode by using bullets instead of stars

--- config.d/30_mode.d/org.el
+++ config.d/30_mode.d/org.el
@@ -1,6 +1,9 @@
 (use-package orgit
   :ensure t)
 
+(use-package org-bullets
+  :ensure t)
+
 ;; setup org mode
 (use-package org
   :ensure org-plus-contrib
@@ -15,3 +18,6 @@
 
 (setq org-default-notes-file "~/tasks.org")
 (define-key global-map "\C-cc" 'org-capture)
+
+(setq org-hide-leading-stars t)
+(add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
