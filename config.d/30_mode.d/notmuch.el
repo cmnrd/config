35cddb08bffbe2a98fa1d1ca88b54380488b4850
Christian Menard <christian.menard@tu-dresden.de>
2017-08-31 15:17:08 +0200

    use icons for many notmuch tags

--- config.d/30_mode.d/notmuch.el
+++ config.d/30_mode.d/notmuch.el
@@ -2,6 +2,62 @@
   :ensure t
   :bind ("C-x m" . notmuch)
   :config
+  ;; use icons to visualize often used tags
+  (setq notmuch-tag-formats `(("unread" (all-the-icons-octicon "mail"))
+                              ("inbox" (all-the-icons-octicon "inbox"))
+                              ("replied" (all-the-icons-octicon "mail-reply"))
+                              ("sent" (all-the-icons-octicon "arrow-up"))
+                              ("to-me" (all-the-icons-octicon "person"))
+                              ("signed" (all-the-icons-octicon "pencil"))
+                              ("work" (all-the-icons-octicon "briefcase"))
+                              ("attachment" (all-the-icons-octicon "package"))))
+
+  ;; helper function to sort tags, this helps to put symbols before other tags
+  (defun my-notmuch-tag-sort (tags)
+    (setq res nil)
+    (setq tail nil)
+    (if (member "unread" tags)
+        (setq tags (remove "unread" tags)
+              res (append res '("unread"))))
+    (if (member "inbox" tags)
+        (setq tags (remove "inbox" tags)
+              res (append res '("inbox"))))
+    (if (member "work" tags)
+        (setq tags (remove "work" tags)
+              res (append res '("work"))))
+    (if (member "signed" tags)
+        (setq tags (remove "signed" tags)
+              res (append res '("signed"))))
+    (if (member "attachement" tags)
+        (setq tags (remove "attachement" tags)
+              res (append res '("attachement"))))
+    (if (member "to-me" tags)
+        (setq tags (remove "to-me" tags)
+              res (append res '("to-me"))))
+    (if (member "replied" tags)
+        (setq tags (remove "replied" tags)
+              tail (append tail '("replied"))))
+    (if (member "sent" tags)
+        (setq tags (remove "sent" tags)
+              tail (append tail '("sent"))))
+    (setq res (append res (sort tags #'string<)))
+    (append res tail))
+
+  ;; replace this notmuch function to use the sort function define above
+  (defun notmuch-tag-format-tags (tags orig-tags &optional face)
+    "Return a string representing formatted TAGS."
+    (let ((face (or face 'notmuch-tag-face))
+          (all-tags (my-notmuch-tag-sort (delete-dups (append tags orig-tags nil)))))
+      (notmuch-apply-face
+       (mapconcat #'identity
+                  ;; nil indicated that the tag was deliberately hidden
+                  (delq nil (mapcar
+                             (apply-partially #'notmuch-tag-format-tag tags orig-tags)
+                             all-tags))
+                  " ")
+       face
+       t)))
+
     (setq notmuch-search-oldest-first nil)
     (setq message-send-mail-function 'message-send-mail-with-sendmail)
     (setq sendmail-program "/usr/local/bin/msmtp-enqueue.sh")
