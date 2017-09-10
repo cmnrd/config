178143521a1183a20a2d896a8ff0f9e4f510556f
Christian Menard <christian.menard@tu-dresden.de>
2017-09-10 17:43:12 +0200

    switch to insert mode when prompting for the commit message

--- config.d/30_mode.d/magit.el
+++ config.d/30_mode.d/magit.el
@@ -5,4 +5,7 @@
   :ensure t
   :bind ("C-x g" . magit-status)
   :config
-    (require 'evil-magit))
+  (require 'evil-magit))
+
+;; switch to insert mode when prompting for the commit message
+(add-hook 'git-commit-mode-hook 'evil-insert-state)
