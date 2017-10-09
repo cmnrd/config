3c34fe3ab437d357c910e5897e7d80f7aa90e1ff
Christian Menard <christian.menard@tu-dresden.de>
2017-10-09 14:42:54 +0200

    improved handling of python docstrings

--- emacs/config.d/30_mode.d/elpy.el
+++ emacs/config.d/30_mode.d/elpy.el
@@ -2,4 +2,17 @@
   :ensure t
   :config
   (elpy-enable)
+  (setq elpy-rpc-backend "jedi")
   (add-hook 'python-mode-hook 'elpy-enable))
+
+(use-package sphinx-doc
+  :ensure t
+  :config
+  (add-hook 'python-mode-hook (lambda ()
+                               (require 'sphinx-doc)
+                               (sphinx-doc-mode t))))
+
+(use-package python-docstring
+  :ensure t
+  :config
+  (add-hook 'python-mode-hook 'python-docstring-mode))
