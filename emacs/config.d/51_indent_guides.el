771426c49e91a4d310eb9345cb0f13db85db1c5a
Christian Menard <christian.menard@tu-dresden.de>
2017-11-07 18:21:47 +0100

    emacs: disable indent highlighting for now
    this makes problems in combination with company mode

--- emacs/config.d/51_indent_guides.el
+++ emacs/config.d/51_indent_guides.el
@@ -1,6 +1,7 @@
-(use-package highlight-indent-guides
-  :ensure t
-  :config
-  (setq highlight-indent-guides-method 'character)
-  (setq elpy-modules (delq 'elpy-module-highlight-indentation elpy-modules))
-  (add-hook 'prog-mode-hook 'highlight-indent-guides-mode))
+;; (use-package highlight-indent-guides
+;;   :ensure t
+;;   :config
+;;   (setq highlight-indent-guides-method 'character)
+;;   (setq elpy-modules (delq 'elpy-module-highlight-indentation elpy-modules))
+;;   (add-hook 'prog-mode-hook 'highlight-indent-guides-mode))
+(setq elpy-modules (delq 'elpy-module-highlight-indentation elpy-modules))
