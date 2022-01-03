d227194612b346f2d7d3564e5f7173015b6fded0
Christian Menard <christian.menard@tu-dresden.de>
2022-01-03 12:48:02 +0100

    fix notmuch version

--- email/remote-notmuch.sh
+++ email/remote-notmuch.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 printf -v ARGS "%q " "$@"
+#echo ${ARGS} >> /tmp/notmuch.txt
 # diable decryption (it took forever, probably smth is/was broken)
 ARGS=`echo ${ARGS} | sed "s/--decrypt=true//g"`
+ARGS=`echo ${ARGS} | sed "s/--format-version=5/--format-version=4/g"`
+#echo ${ARGS} >> /tmp/notmuch.txt
 exec timeout 10 ssh batou notmuch ${ARGS}
