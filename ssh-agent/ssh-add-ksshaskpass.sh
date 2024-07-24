cdbd87f84ebc5db10d3a2063821fedcb1c6248a0
Christian Menard <christian@xronos.com>
2024-07-24 14:29:32 +0200

    update ssh config

--- ssh-agent/ssh-add-ksshaskpass.sh
+++ ssh-agent/ssh-add-ksshaskpass.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
+
+sleep 10
+
+# Wait for kwallet
+kwallet-query -l kdewallet > /dev/null
+
+export SSH_AUTH_SOCK="${XDG_RUNTIME_DIR}/ssh-agent.socket"
 export SSH_ASKPASS="/usr/bin/ksshaskpass"
 ssh-add
